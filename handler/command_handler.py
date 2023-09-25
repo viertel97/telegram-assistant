212c5c40788a0cfbd5e55f7a5dfbecb805efe19f
viertel97 <janik.viertel@outlook.com>
2023-09-25 11:37:47 +0200

    fixed import error

--- handler/command_handler.py
+++ handler/command_handler.py
@@ -2,16 +2,11 @@ from telegram import Update
 from telegram.ext import CallbackContext
 
 from services.grabber_service import dump_todoist_to_monica as dump
-from services.transcriber_service import separate_audibooks
 from services.wol_service import start_pc_via_wol
 
 
 async def wol(update: Update, context: CallbackContext):
     await start_pc_via_wol(update, context)
-
-
-async def separate(update: Update, context: CallbackContext):
-    await separate_audibooks(update, context)
 
 
 async def dump_todoist_to_monica(update: Update, context: CallbackContext):
