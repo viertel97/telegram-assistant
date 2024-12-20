16176547b1b6a20cda9d34fc83e7841f4da11064
viertel97 <janik.viertel@outlook.com>
2024-12-20 15:12:06 +0100

    updated prompt

--- services/llm_service.py
+++ services/llm_service.py
@@ -19,7 +19,9 @@ chat = ChatGroq(temperature=0.5, model_name="llama-3.3-70b-versatile",
 parser = JsonOutputParser()
 
 prompt = PromptTemplate(
-    template="""You are summarizing a list of personal notes. Summarize each note individually, ensuring it is concise and retains its main meaning. Provide the output as a list of unique summarized notes in the same order as the input list. Don't translate the notes and use the language provided in the notes. Don't use any special characters and keep the summary as short as possible.\n{format_instructions}\n{notes}\n""",
+    template="""
+You are summarizing a list of personal notes. Summarize each note individually, ensuring it is concise and retains its main meaning.
+Provide the output as a list of unique summarized notes in the same order as the input list. Don't translate the notes and use the language provided in the notes. Don't use any special characters and keep the summary as short as possible. Also do not shrink the number of entries in the list. When you receive a list with 5 values then you also return a list with 5 values.\n{format_instructions}\n{notes}\n""",
     input_variables=["notes"],
     partial_variables={"format_instructions": parser.get_format_instructions()},
 )
