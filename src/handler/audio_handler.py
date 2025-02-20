62024ce27eed6e31292c4b9b26812cae0c2955c0
Janik Viertel <janik.viertel@outlook.com>
2025-02-20 10:26:31 +0100

    added fix for mp3 files

--- src/handler/audio_handler.py
+++ src/handler/audio_handler.py
@@ -25,15 +25,19 @@ async def handle_audio(update: Update, context: CallbackContext):
 	if not mime_type:
 		if os.path.splitext(update.message.document.file_name)[1] in ["ogg", "oga"]:
 			mime_type = "audio/ogg"
+		if os.path.splitext(update.message.document.file_name)[1] in ["mp3", "m4a"]:
+			mime_type = "audio/mpeg"
 	if not performer:
 		performer = "unknown"
 	logger.info("received audio " + mime_type + " by " + performer)
 	file_path, file_name = await prepairing_audio(update, context)
 
 	if mime_type == "audio/mpeg" and performer == "Easy Voice Recorder Pro":
+		await update.message.reply_text("start get_meditation_logs")
 		done_message = get_meditation_logs(file_path, file_name)
 		await update.message.reply_text(done_message)
-	elif mime_type == "audio/ogg":
+	elif mime_type == "audio/ogg" or mime_type == "audio/mpeg":
+		await update.message.reply_text("start transcribe_groq")
 		wav_converted_file_path = convert_to_wav(file_path)
 		transcription_list = await transcribe_groq(wav_converted_file_path,
 												   file_function=update.message.reply_document,
@@ -41,5 +45,7 @@ async def handle_audio(update: Update, context: CallbackContext):
 		recognized_text = " ".join(transcription_list)
 		await send_long_message(recognized_text, update.message.reply_text)
 		os.remove(wav_converted_file_path)
+		await update.message.reply_text("end transcribe_groq")
 	else:
+		await update.message.reply_text("unsupported mime type: " + str(mime_type))
 		logger.error("unsupported mime type: " + str(mime_type))
